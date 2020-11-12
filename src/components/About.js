48b497ab1cd3333112bc4d46391d7100527ad2a7
miryalakavya <45136999+miryalakavya@users.noreply.github.com>
2020-11-13 01:00:12 +0530

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const About = () => {
+  return (
+    <div className="container">
+      <h1>About</h1>
+      <p>This is the CRUD Operations On the Employeee!!! Like CREATE!!DELETE!!EDIT!!ADD</p>
+    </div>
+  );
+};
+
+
+export default About;
---
+++ src/components/landing/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+
+const Header = () => {
+    return (
+        <header>
+            <div className="jumbotron jumbotron-fluid bg-info text-white text-center">
+                <div className="container">
+                    <h1 className="display-4">ReactJS CRUD</h1>
+                 
+                </div>
+            </div>
+        </header>
+    );
+};
+
+
+export default Header;
